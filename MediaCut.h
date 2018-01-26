7a83f97ef1ec85c18becf84049b15f180bf62f8f
a.c.max <geey11u@126.com>
2018-01-26 22:38:26 +0800

    cut well
    Signed-off-by: a.c.max <geey11u@126.com>

---
+++ MediaCut.h
@@ -0,0 +1,27 @@
+#ifndef __MEDIA_CUT__
+#define __MEDIA_CUT__
+
+#include <iostream>
+#include <queue>
+#include <cassert>
+
+extern "C"{
+#include "libavformat/avformat.h"
+}
+
+class MediaCut{
+  
+  int64_t vpts=0, apts=0;
+  int openInput();
+  int openOutput();
+  std::queue<AVPacket*> vQueue;
+  std::queue<AVPacket*> aQueue;
+  AVFormatContext *inFmtCtx;
+  AVFormatContext* outFmtCtx;
+  int inVideoIndex, inAudioIndex;
+public:
+  MediaCut();
+  void cut(int64_t begin, int64_t end);
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "MediaCut.h"
+
+int main(int argc, char **argv) {
+    
+  MediaCut mc;
+  mc.cut(20, 20);
+  
+  return 0;
+}
